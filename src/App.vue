83e00d770149f3b3cba406b1a21eabb0eb5ad8ac
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-25 01:23:54 +0330

    Add auth base component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <auth-base>hierere</auth-base>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AuthBase from "./components/modules/auth/AuthBase.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { AuthBase },
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/modules/auth/AuthBase.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex w-full h-screen">
+    <div
+      class="basis-2/5 bg-cornflower-blue-400 px-10 py-32 text-white flex flex-col justify-between"
+    >
+      <h1 class="text-4xl">Storage</h1>
+
+      <div>
+        <p class="text-5xl font-bold">Manage your files the best way</p>
+        <p>Awesome, we've created the perfect place for you to store all your documents.</p>
+      </div>
+
+      <div class="flex justify-center">
+        <img src="../../../assets/search-files-illustration.png" class="w-2/3" />
+      </div>
+    </div>
+    <div class="flex-grow px-20">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthBase",
+};
+</script>
