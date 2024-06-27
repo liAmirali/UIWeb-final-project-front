3bbf69ad779f519d3ecd69a8159d0978de505913
asalkhaef <asal.khaef@gmail.com>
2024-06-27 21:08:26 +0330

    Add PeopleItem and PeopleContainer component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <!-- <auth-base><verify-email email="amirali@gmail.com" /></auth-base> -->
-  <auth-base><login-form /></auth-base>
+  <!-- <auth-base><login-form /></auth-base> -->
   <!-- <the-header></the-header> -->
   <!-- <auth-base><create-account-form /></auth-base> -->
   <!-- <object-card></object-card> -->
   <!-- <object-menu fileName="Asal.pdf" accessType="viewer"></object-menu> -->
+  <people-container></people-container>
 </template>
 
 <script>
+// import BackButton from "./components/common/buttons/BackButton.vue";
+// import PeopleItem from "./components/modules/people/PeopleItem.vue";
+import PeopleContainer from "./components/modules/people/PeopleContainer.vue";
 // import ObjectMenu from "./components/modules/objects/ObjectMenu.vue";
 // import ObjectCard from "./components/modules/objects/ObjectCard.vue";
-import AuthBase from "./components/modules/auth/AuthBase.vue";
+// import AuthBase from "./components/modules/auth/AuthBase.vue";
 // import CreateAccountForm from "./components/modules/auth/CreateAccountForm.vue";
-import LoginForm from "./components/modules/auth/LoginForm.vue";
+// import LoginForm from "./components/modules/auth/LoginForm.vue";
 // import TheHeader from "./components/modules/objects/TheHeader.vue";
 // import VerifyEmail from "./components/modules/auth/VerifyEmail.vue";
 
 export default {
   name: "App",
   components: {
+    // PeopleItem,
+    // BackButton,
+    PeopleContainer,
     // ObjectMenu,
-    AuthBase,
+    // AuthBase,
     // CreateAccountForm,
     // VerifyEmail,
-    LoginForm,
+    // LoginForm,
     //TheHeader,
     // ObjectCard,
   },
---
+++ src/components/modules/people/PeopleItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex gap-x-3 items-center">
+    <div class="size-8">
+      <img class="rounded-full" :src="pictureUrl" alt="" />
+    </div>
+    <div class="flex flex-col">
+      <p class="text-sm font-semibold">{{ name }}</p>
+      <p class="text-sm text-gray-400">{{ email }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pictureUrl: String,
+    name: String,
+    email: String,
+  },
+};
+</script>
+
+<style>
+</style>
