0f9728ac25e0855b9a8537d8ee143a23b4e2cabe
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-27 20:18:30 +0330

    Add objects page; Fix a bug in object card file addresses

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import { createRouter, createMemoryHistory } from "vue-router";
 import LoginPage from "./components/modules/auth/LoginPage.vue";
 import RegisterPage from "./components/modules/auth/RegisterPage.vue";
 import VerifyEmail from "./components/modules/auth/VerifyEmail.vue";
+import ObjectsPage from "./components/modules/objects/ObjectsPage.vue"
 
 const routes = [
   { path: "/login", component: LoginPage },
   { path: "/register", component: RegisterPage },
   { path: "/verify", component: VerifyEmail },
+  { path: "", component: ObjectsPage },
 ];
 
 const router = createRouter({
