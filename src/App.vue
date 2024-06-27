b02cf7ad26da0bd23eab81013abd7dcefe0435c5
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-27 20:11:08 +0330

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
-  <verify-email-page />
+  <router-view />
 </template>
 
 <script>
-import VerifyEmailPage from "./components/modules/auth/VerifyEmailPage.vue";
-// import LoginPage from "./components/modules/auth/LoginPage.vue";
-// import RegisterPage from "./components/modules/auth/RegisterPage.vue";
 export default {
   name: "App",
-  components: {
-    // LoginPage,
-    // RegisterPage,
-    VerifyEmailPage,
-  },
 };
 </script>
 
 <style>
-CreateAccountForm #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+import { createRouter, createMemoryHistory } from "vue-router";
 
-createApp(App).mount('#app')
+import LoginPage from "./components/modules/auth/LoginPage.vue";
+import RegisterPage from "./components/modules/auth/RegisterPage.vue";
+import VerifyEmail from "./components/modules/auth/VerifyEmail.vue";
+
+const routes = [
+  { path: "/login", component: LoginPage },
+  { path: "/register", component: RegisterPage },
+  { path: "/verify", component: VerifyEmail },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
