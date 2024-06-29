f8d27d12f0b673c09d1c78570607f4436f5e5c3e
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-29 19:09:15 +0330

    Read login form inputs and call login API

--- src/components/common/buttons/AppButton.vue
+++ src/components/common/buttons/AppButton.vue
@@ -2,6 +2,7 @@
   <button
     class="bg-cornflower-blue-400 hover:bg-cornflower-blue-700 text-white font-bold py-3 px-4 rounded-full transition-all"
     :class="{ 'w-full': fullWidth }"
+    :type="type"
   >
     <slot></slot>
   </button>
@@ -11,6 +12,10 @@
 export default {
   props: {
     fullWidth: Boolean,
+    type: {
+      type: String,
+      default: "button",
+    },
   },
   name: "AppButton",
 };
--- src/components/modules/auth/LoginForm.vue
+++ src/components/modules/auth/LoginForm.vue
@@ -1,36 +1,62 @@
 <template>
-  <div class="flex flex-col justify-between h-[70%]">
+  <form @submit.prevent="onFormSubmit" class="flex flex-col justify-between h-[70%]">
     <h1 class="font-bold text-5xl">Login</h1>
     <div class="space-y-6">
       <app-input
+        name="username"
         label="Username | Email"
         placeholder="Enter your Username or Email"
       />
       <app-input
+        name="password"
         label="Password"
         placeholder="Enter your Password"
         type="password"
       />
+      <p v-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
     </div>
     <div class="space-y-6">
-      <app-button :fullWidth="true">Login</app-button>
+      <app-button :fullWidth="true" type="submit">Login</app-button>
       <p class="text-center">
         Don't have an account?
         <a class="hover:underline text-picton-blue-300 cursor-pointer">Create Account.</a>
       </p>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
 import AppButton from "@/components/common/buttons/AppButton.vue";
 import AppInput from "@/components/common/inputs/AppInput.vue";
+import axios from "axios";
 
 export default {
   name: "LoginForm",
+  data() {
+    return { errorMessage: "" };
+  },
   components: {
     AppInput,
     AppButton,
+  },
+  methods: {
+    onFormSubmit(event) {
+      this.errorMessage = "";
+
+      const username = event.target.username.value;
+      const password = event.target.password.value;
+
+      console.log(username, password);
+
+      axios
+        .post("http://localhost:8000/api/auth/login/", { username, password })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          this.errorMessage = error.response.data.detail;
+        });
+    },
   },
 };
 </script>
