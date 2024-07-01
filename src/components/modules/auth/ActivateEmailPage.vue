662427c0d63296c662e8d6b2d62f74b4ad842039
asalkhaef <asal.khaef@gmail.com>
2024-07-02 02:04:43 +0330

    Add api call for email activation

--- src/components/modules/auth/ActivateEmailPage.vue
+++ src/components/modules/auth/ActivateEmailPage.vue
@@ -5,9 +5,44 @@
 <script>
 import AuthBase from "./AuthBase.vue";
 import ActivateEmail from "./ActivateEmail.vue";
+import { fetcher } from "@/config/api";
 
 export default {
   components: { AuthBase, ActivateEmail },
+  mounted() {
+    this.activateAccount();
+  },
+  methods: {
+    activateAccount() {
+      // Function to get query parameters
+      const queryParams = new URLSearchParams(window.location.search);
+      const uid = queryParams.get("uid");
+      const token = queryParams.get("token");
+
+      if (uid && token) {
+        fetcher
+          .get("/auth/login/", {
+            params: {
+              uid: uid,
+              token: token,
+            },
+          })
+          .then((response) => {
+            // Handle success
+            console.log("Activation successful:", response.data);
+            this.message = "Email verified successfully";
+          })
+          .catch((error) => {
+            // Handle error
+            console.error("Activation failed:", error.response.data);
+            this.error = "Activation link is invalid";
+          });
+      } else {
+        console.error("Invalid query parameters");
+        this.error = "Invalid activation link";
+      }
+    },
+  },
 };
 </script>
 
