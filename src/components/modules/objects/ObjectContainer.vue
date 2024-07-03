842e4652fd7d8b9199faf3ae0f697b183269d1a4
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-07-04 03:16:25 +0330

    Add file pagination

--- src/components/modules/objects/ObjectContainer.vue
+++ src/components/modules/objects/ObjectContainer.vue
@@ -11,12 +11,22 @@
       <object-card v-for="file in files" :key="file.id" :file="file" />
     </div>
   </div>
+
+  <div class="bg-catskill-white my-5 mx-5 rounded-xl py-8 px-8">
+    <div class="flex mx-auto">
+      <div v-if="page > 1" @click="page = page - 1">&lt;</div>
+      <div>{{ page }} / {{ pageCount }}</div>
+      <div v-if="pageCount > page" @click="page = page + 1">&gt;</div>
+    </div>
+  </div>
 </template>
 
 <script>
 import ObjectCard from "@/components/modules/objects/ObjectCard.vue";
 import { fetcher } from "@/config/api";
 import { humanReadableSize } from "@/helpers/prettifiers";
+
+const PAGE_SIZE = 3;
 
 export default {
   name: "ObjectContainer",
@@ -26,18 +36,30 @@ export default {
   data() {
     return {
       files: [],
+      page: 1,
+      pageCount: 1,
     };
   },
   computed: {
     getTotalObjectsSize() {
       return humanReadableSize(this.files.reduce((acc, file) => acc + file.size, 0));
     },
+    filesCount() {
+      return this.files.length;
+    },
   },
   methods: {
     fetchList() {
-      fetcher("/objects/list/")
+      fetcher
+        .get("/objects/list/", {
+          params: {
+            page: this.page,
+            page_size: PAGE_SIZE,
+          },
+        })
         .then((res) => {
           this.files = res.data.results;
+          this.pageCount = Math.ceil(res.data.count / PAGE_SIZE);
         })
         .catch(() => {
           alert("Error fetching objects list.");
@@ -47,6 +69,11 @@ export default {
   mounted() {
     this.fetchList();
   },
+  watch: {
+    page() {
+      this.fetchList();
+    },
+  }
 };
 </script>
 
